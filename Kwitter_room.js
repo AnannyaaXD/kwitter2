3b8949dc684bd283f1495499b7e74e4f99e60d1e
IamElsa1234 <100927178+IamElsa1234@users.noreply.github.com>
2023-03-14 10:35:13 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyARk_liJ4NgoNa3sKr3J5ge1Tt_dcjLoi4",
+    authDomain: "kwitter-4d021.firebaseapp.com",
+    projectId: "kwitter-4d021",
+    storageBucket: "kwitter-4d021.appspot.com",
+    messagingSenderId: "9502702622",
+    appId: "1:9502702622:web:12504051a4ba3ec013c20f",
+    measurementId: "G-TLKF2HS8NP"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
